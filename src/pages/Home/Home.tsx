37d0b5afa1ff21c8f4d9bd799b93d731096b0feb
Enzo Rossi Ribeiro da Silva <enzorrsilva@gmail.com>
2026-02-09 20:46:01 -0300

    feat: creating hero component ui

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
-import Hero from "./sections/Hero/hero"
+import NavBar from "../../components/NavBar/NavBar"
+import Hero from "./sections/Hero/Hero"
 
 const Home = () => {
 
   return (
     <>
+        <NavBar />
         <Hero />
     </>
   )
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,21 +1,91 @@
-import { styled } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.jpg.jpeg";
+import DownloadIcon from "@mui/icons-material/Download";
+import EmailIcon from "@mui/icons-material/Email";
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
+const StyledHero = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+}));
 
+const StyledImg = styled("img")(({ theme }) => ({
+  width: "80%",
+  aspectRatio: "1 / 1",
+  objectFit: "cover",
+  borderRadius: "50%",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+}));
 
 const Hero = () => {
-
-    const StyledHero = styled("div")(() => ({
-       backgroundColor: "black"
-    }))
-
   return (
     <>
-        <StyledHero>
-            ola
-        </StyledHero>
-         
+      <StyledHero>
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12, md: 5 }}>
+              <Box position="relative">
+                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                  <AnimatedBackground />
+                </Box>
+                <Box position="relative" textAlign="center">
+                  <StyledImg src={Avatar} />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid size={{ xs: 12, md: 7 }}>
+              <Typography
+                color="primary.contrastText"
+                variant="h1"
+                textAlign="center"
+              >
+                Enzo Rossi
+              </Typography>
+              <Typography
+                color="primary.contrastText"
+                variant="h2"
+                textAlign="center"
+                pb={2}
+              >
+                I'm a Software Engineer
+              </Typography>
+              <Grid
+                container
+                display="flex"
+                justifyContent="center"
+                spacing={3}
+                pt={3}
+              >
+                <Grid
+                  size={{ xs: 12, md: 4 }}
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <StyledButton>
+                    <DownloadIcon />
+                    <Typography>Download CV</Typography>
+                  </StyledButton>
+                </Grid>
+                <Grid
+                  size={{ xs: 12, md: 4 }}
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <StyledButton>
+                    <EmailIcon />
+                    <Typography>Contact Me</Typography>
+                  </StyledButton>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </StyledHero>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
